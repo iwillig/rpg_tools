7fcf43950ad5aa98c63cf39b24c1c16d8c752333
Ivan Willig <iwillig@gmail.com>
2023-09-26 22:02:24 -0400

    Add person and character tables

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from database import load_engine, create_all_tables
 
 
 def hello_world(request):
     return Response('Hello World!')
 
 
-if __name__ == '__main__':
+def load_app():
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
+        ## Is this the right way to handle local vars in python?
         app = config.make_wsgi_app()
-        
+    return app
+
+if __name__ == '__main__':
+    app = load_app()
+    engine = load_engine()
+    create_all_tables(engine)
+    print("server-start", engine)
+
     server = make_server('0.0.0.0', 6543, app)
+
     server.serve_forever()
---
+++ database.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import registry
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Person(Base):
+    __tablename__ = "person"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+
+
+class Character(Base):
+    __tablename__ = "character"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+
+def create_all_tables(engine):
+    Base.metadata.create_all(engine)
+
+
+def load_engine():
+    engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+    return engine
